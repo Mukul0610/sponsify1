58e735c0986d97131050feed7cf3e0de6b250ac4
Mukul Rai <mukul93028@gmail.com>
2024-05-28 19:58:56 +0530

    error5

--- lib/utils.ts
+++ lib/utils.ts
@@ -28,61 +28,12 @@ export const handleError = (error: unknown) => {
 };
 
 // PLACEHOLDER LOADER - while image is transforming
-const shimmer = (w: number, h: number) => `
-<svg width="${w}" height="${h}" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-  <defs>
-    <linearGradient id="g">
-      <stop stop-color="#7986AC" offset="20%" />
-      <stop stop-color="#68769e" offset="50%" />
-      <stop stop-color="#7986AC" offset="70%" />
-    </linearGradient>
-  </defs>
-  <rect width="${w}" height="${h}" fill="#7986AC" />
-  <rect id="r" width="${w}" height="${h}" fill="url(#g)" />
-  <animate xlink:href="#r" attributeName="x" from="-${w}" to="${w}" dur="1s" repeatCount="indefinite"  />
-</svg>`;
 
-const toBase64 = (str: string) =>
-  typeof window === "undefined"
-    ? Buffer.from(str).toString("base64")
-    : window.btoa(str);
-
-export const dataUrl = `data:image/svg+xml;base64,${toBase64(
-  shimmer(1000, 1000)
-)}`;
 // ==== End
 
-// FORM URL QUERY
-export const formUrlQuery = ({
-  searchParams,
-  key,
-  value,
-}: FormUrlQueryParams) => {
-  const params = { ...qs.parse(searchParams.toString()), [key]: value };
 
-  return `${window.location.pathname}?${qs.stringify(params, {
-    skipNulls: true,
-  })}`;
-};
 
 // REMOVE KEY FROM QUERY
-export function removeKeysFromQuery({
-  searchParams,
-  keysToRemove,
-}: RemoveUrlQueryParams) {
-  const currentUrl = qs.parse(searchParams);
-
-  keysToRemove.forEach((key) => {
-    delete currentUrl[key];
-  });
-
-  // Remove null or undefined values
-  Object.keys(currentUrl).forEach(
-    (key) => currentUrl[key] == null && delete currentUrl[key]
-  );
-
-  return `${window.location.pathname}?${qs.stringify(currentUrl)}`;
-}
 
 // DEBOUNCE
 export const debounce = (func: (...args: any[]) => void, delay: number) => {
