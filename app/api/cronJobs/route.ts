a4a6bb0c282476dffd87f288442089a3030f36fe
Mukul Rai <mukul93028@gmail.com>
2024-05-28 19:39:01 +0530

    error2

--- app/api/cronJobs/route.ts
+++
@@ -1,7 +0,0 @@
-import { NextApiRequest, NextApiResponse } from 'next';
-import {updateDocuments} from "../../../lib/ScheduleTask"
-
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  updateDocuments(); // Ensure the cron job starts
-  res.status(200).json({ message: 'Cron job started' });
-}
